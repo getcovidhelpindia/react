a237e4bc63ed302e5a9bb53bd8f7668b420b5872
DesignrKnight <notify.abelmathew@gmail.com>
2021-04-20 23:27:10 +0530

    Add theme for Ag-grid

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,22 +4,27 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
+import useDarkMode from 'use-dark-mode';
+
 const Table = () => {
 	const [gridApi, setGridApi] = useState(null);
 	const [gridColumnApi, setGridColumnApi] = useState(null);
 
 	const [rowData, setRowData] = useState([]);
 
+	let storageKey = 'darkMode';
+	let localStorageTheme = null;
+	try {
+		localStorageTheme = localStorage.getItem(storageKey);
+	} catch (err) {}
+
 	useEffect(() => {
 		fetch('https://www.ag-grid.com/example-assets/row-data.json')
 			.then((result) => result.json())
 			.then((rowData) => setRowData(rowData));
 	}, []);
-	return (
-		<div
-			className="ag-theme-alpine"
-			style={{ height: '20rem', width: '80%', marginLeft: '10%', marginTop: '2%', marginRight: '5%' }}
-		>
+	function AgGridTab() {
+		return (
 			<AgGridReact rowData={rowData}>
 				<AgGridColumn field="make" filter={true} sortable={true}></AgGridColumn>
 				<AgGridColumn field="model"></AgGridColumn>
@@ -31,6 +36,21 @@ const Table = () => {
 				<AgGridColumn field="model"></AgGridColumn>
 				<AgGridColumn field="price"></AgGridColumn>
 			</AgGridReact>
+		);
+	}
+	return localStorageTheme ? (
+		<div
+			className="ag-theme-alpine-dark"
+			style={{ height: '20rem', width: '80%', marginLeft: '10%', marginTop: '2%', marginRight: '5%' }}
+		>
+			<AgGridTab />
+		</div>
+	) : (
+		<div
+			className="ag-theme-alpine"
+			style={{ height: '20rem', width: '80%', marginLeft: '10%', marginTop: '2%', marginRight: '5%' }}
+		>
+			<agGrid />
 		</div>
 	);
 };
