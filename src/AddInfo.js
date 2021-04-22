e0cddfffed47a20158c5b623021973ed48a73b12
DesignrKnight <notify.abelmathew@gmail.com>
2021-04-22 09:04:51 +0530

    Add Info UI Update for Success Failure Loading

--- src/AddInfo.js
+++ src/AddInfo.js
@@ -10,12 +10,43 @@ import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import Footer from './components/Footer';
 import { StateAndDistrict } from './utils/statesAndDistrict';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green, red } from '@material-ui/core/colors';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		'& .MuiTextField-root': {
 			margin: theme.spacing(1),
 			width: '25ch',
+		},
+	},
+	buttonProgress: {
+		color: green[500],
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		marginTop: -12,
+		marginLeft: -12,
+	},
+	rootButton: {
+		display: 'flex',
+		alignItems: 'center',
+	},
+	wrapper: {
+		margin: theme.spacing(1),
+		position: 'relative',
+	},
+	buttonSuccess: {
+		backgroundColor: green[500],
+		'&:hover': {
+			backgroundColor: green[700],
+		},
+	},
+	buttonFailure: {
+		backgroundColor: red[500],
+		'&:hover': {
+			backgroundColor: red[700],
 		},
 	},
 	formControl: {
@@ -28,7 +59,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function MultilineTextFields({ darkMode }) {
-	console.log(StateAndDistrict.states.length);
 	const classes = useStyles();
 	const [name, setName] = React.useState('');
 	const [contact, setContact] = React.useState('');
@@ -38,6 +68,14 @@ export default function MultilineTextFields({ darkMode }) {
 	const [indiaState, setIndiaState] = React.useState(-1);
 	const [indiaDistrict, setIndiaDistrict] = React.useState(-1);
 	const [resourceType, setResourceType] = React.useState(-1);
+	const [loading, setLoading] = React.useState(false);
+	const [success, setSuccess] = React.useState(false);
+	const [failure, setFailure] = React.useState(false);
+	const buttonClassname = clsx({
+		[classes.buttonSuccess]: success,
+		[classes.buttonFailure]: failure,
+	});
+
 	const handleChangeState = (event) => {
 		setIndiaState(event.target.value);
 		setIndiaDistrict(-1);
@@ -67,31 +105,65 @@ export default function MultilineTextFields({ darkMode }) {
 		event.preventDefault();
 	};
 	const dataSubmit = (event) => {
-		const payload = {
-			state: StateAndDistrict.states[indiaState].state,
-			district: StateAndDistrict.states[indiaState].districts[indiaDistrict],
-			type: resourceType.toString(),
-			info: {
-				name: name,
-				contact: contact,
-				description: info,
-				location: location,
-			},
-			source: source,
-			createdAt: new Date(),
-			isApproved: true,
-			isFlagged: false,
-			Usefulness: 0,
-			isHidden: false,
-		};
-		console.log(JSON.stringify(payload));
-		const requestOptions = {
-			method: 'POST',
-			body: JSON.stringify(payload),
-		};
-		fetch('https://api.getcovidhelp.in/addData', requestOptions)
-			.then((res) => res.json())
-			.then((result) => console.log(result));
+		if (!loading) {
+			setLoading(true);
+		}
+		try {
+			const payload = {
+				state: StateAndDistrict.states[indiaState].state,
+				district: StateAndDistrict.states[indiaState].districts[indiaDistrict],
+				type: resourceType.toString(),
+				info: {
+					name: name,
+					contact: contact,
+					description: info,
+					location: location,
+				},
+				source: source,
+				createdAt: new Date(),
+				isApproved: true,
+				isFlagged: false,
+				Usefulness: 0,
+				isHidden: false,
+			};
+			console.log(JSON.stringify(payload));
+			const requestOptions = {
+				method: 'POST',
+				body: JSON.stringify(payload),
+			};
+			fetch('https://api.getcovidhelp.in/addData', requestOptions)
+				.then((res) => res.json())
+				.then((result) => {
+					console.log('response', result);
+					if (result.success === 'true') {
+						setLoading(false);
+						setSuccess(true);
+						setTimeout(() => {
+							setName('');
+							setContact('');
+							setLocation('');
+							setInfo('');
+							setSource('');
+							setIndiaState(-1);
+							setIndiaDistrict(-1);
+							setResourceType(-1);
+							setSuccess(false);
+						}, 2000);
+					} else {
+						setLoading(false);
+						setFailure(true);
+						setTimeout(() => {
+							setFailure(false);
+						}, 2000);
+					}
+				});
+		} catch (err) {
+			setFailure(true);
+			setLoading(false);
+			setTimeout(() => {
+				setFailure(false);
+			}, 2000);
+		}
 	};
 	const theme = {
 		palette: {
@@ -104,70 +176,80 @@ export default function MultilineTextFields({ darkMode }) {
 	return (
 		<>
 			<ThemeProvider theme={themeConfig}>
-				<div style={{ height: '30rem', width: '85%', marginLeft: '10%', marginTop: '2%', marginRight: '3%' }}>
-					<FormControl required variant="outlined" className={classes.formControl}>
-						<InputLabel id="demo-simple-select-required-label">State</InputLabel>
-						<Select
-							labelId="demo-simple-select-required-label"
-							id="demo-simple-select-required"
-							value={indiaState}
-							onChange={handleChangeState}
-							className={classes.selectEmpty}
-						>
-							<MenuItem value={-1}>
-								<em>None</em>
-							</MenuItem>
-							{StateAndDistrict.states.map((item, index) => (
-								<MenuItem value={index}>{item.state}</MenuItem>
-							))}
-						</Select>
-						<FormHelperText>Required</FormHelperText>
-					</FormControl>
+				<div
+					style={{
+						height: '30rem',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'left',
+						marginLeft: '10%',
+						marginTop: '2%',
+					}}
+				>
+					<div>
+						<FormControl required variant="outlined" className={classes.formControl}>
+							<InputLabel id="demo-simple-select-required-label">State</InputLabel>
+							<Select
+								labelId="demo-simple-select-required-label"
+								id="demo-simple-select-required"
+								value={indiaState}
+								onChange={handleChangeState}
+								className={classes.selectEmpty}
+							>
+								<MenuItem value={-1}>
+									<em>None</em>
+								</MenuItem>
+								{StateAndDistrict.states.map((item, index) => (
+									<MenuItem value={index}>{item.state}</MenuItem>
+								))}
+							</Select>
+							<FormHelperText>Required</FormHelperText>
+						</FormControl>
 
-					<FormControl required variant="outlined" className={classes.formControl}>
-						<InputLabel id="demo-simple-select-required-label">District</InputLabel>
-						<Select
-							labelId="demo-simple-select-required-label"
-							id="demo-simple-select-required"
-							value={indiaDistrict}
-							onChange={handleChangeDistrict}
-							className={classes.selectEmpty}
-						>
-							<MenuItem value={-1}>
-								<em>None</em>
-							</MenuItem>
-							{indiaState >= 0 ? (
-								StateAndDistrict.states[indiaState].districts.map((item, index) => (
-									<MenuItem value={index}>{item}</MenuItem>
-								))
-							) : (
-								<div />
-							)}
-						</Select>
-						<FormHelperText>Required</FormHelperText>
-					</FormControl>
+						<FormControl required variant="outlined" className={classes.formControl}>
+							<InputLabel id="demo-simple-select-required-label">District</InputLabel>
+							<Select
+								labelId="demo-simple-select-required-label"
+								id="demo-simple-select-required"
+								value={indiaDistrict}
+								onChange={handleChangeDistrict}
+								className={classes.selectEmpty}
+							>
+								<MenuItem value={-1}>
+									<em>None</em>
+								</MenuItem>
+								{indiaState >= 0 ? (
+									StateAndDistrict.states[indiaState].districts.map((item, index) => (
+										<MenuItem value={index}>{item}</MenuItem>
+									))
+								) : (
+									<div />
+								)}
+							</Select>
+							<FormHelperText>Required</FormHelperText>
+						</FormControl>
 
-					<FormControl required variant="outlined" className={classes.formControl}>
-						<InputLabel id="demo-simple-select-required-label">Resource Type</InputLabel>
-						<Select
-							labelId="demo-simple-select-required-label"
-							id="demo-simple-select-required"
-							value={resourceType}
-							onChange={handleChangeResourceType}
-							className={classes.selectEmpty}
-						>
-							<MenuItem value={-1}>
-								<em>None</em>
-							</MenuItem>
-							<MenuItem value={0}>Oxygen</MenuItem>
-							<MenuItem value={1}>Medicines</MenuItem>
-							<MenuItem value={2}>Plasma Donor</MenuItem>
-							<MenuItem value={3}>Hospital Bed</MenuItem>
-							<MenuItem value={4}>Testing Facility</MenuItem>
-						</Select>
-						<FormHelperText>Required</FormHelperText>
-					</FormControl>
-
+						<FormControl required variant="outlined" className={classes.formControl}>
+							<InputLabel id="demo-simple-select-required-label">Resource Type</InputLabel>
+							<Select
+								labelId="demo-simple-select-required-label"
+								id="demo-simple-select-required"
+								value={resourceType}
+								onChange={handleChangeResourceType}
+								className={classes.selectEmpty}
+							>
+								<MenuItem value={-1}>
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={0}>Oxygen</MenuItem>
+								<MenuItem value={1}>Medicines</MenuItem>
+								<MenuItem value={2}>Plasma Donor</MenuItem>
+								<MenuItem value={3}>Hospital Bed</MenuItem>
+								<MenuItem value={4}>Testing Facility</MenuItem>
+							</Select>
+							<FormHelperText>Required</FormHelperText>
+						</FormControl>
+					</div>
 					<form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmitForm}>
 						<TextField
 							id="outlined-multiline-flexible"
@@ -209,9 +291,20 @@ export default function MultilineTextFields({ darkMode }) {
 							variant="outlined"
 						/>
 					</form>
-					<Button variant="contained" color="primary" onClick={dataSubmit}>
-						Submit
-					</Button>
+					<div className={classes.rootButton}>
+						<div className={classes.wrapper}>
+							<Button
+								variant="contained"
+								color="primary"
+								onClick={dataSubmit}
+								disabled={loading}
+								className={buttonClassname}
+							>
+								Submit
+							</Button>
+							{loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+						</div>
+					</div>
 				</div>
 			</ThemeProvider>
 			<Footer />
