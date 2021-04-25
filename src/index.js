d7ed23b44ad27f351dc3fc7805781e54ecffd6bf
Ritesh Patil <riteshsp2000@gmail.com>
2021-04-25 23:50:45 +0530

    Create separate loadScript utility function

--- src/index.js
+++ src/index.js
@@ -1,45 +1,38 @@
 import { retry } from './utils/commonFunctions';
 
+// Libraries
 import { Suspense, lazy, StrictMode } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Utilities
+import loadScript from './utils/loadScript';
+
+// Components
 const App = lazy(() => retry(() => import('./App')));
 const rootElement = document.getElementById('root');
 
 const main = () =>
-	render(
-		<Suspense fallback={<div />}>
-			<Router>
-				<StrictMode>
-					<App />
-				</StrictMode>
-			</Router>
-		</Suspense>,
-		rootElement
-	);
+  render(
+    <Suspense fallback={<div />}>
+      <Router>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </Router>
+    </Suspense>,
+    rootElement
+  );
 
 const browserSupportsAllFeatures = () => {
-	return window.requestIdleCallback && window.IntersectionObserver;
-};
-
-const loadScript = (src, done) => {
-	const js = document.createElement('script');
-	js.src = src;
-	js.onload = function () {
-		done();
-	};
-	js.onerror = function () {
-		done(new Error('Failed to load script ' + src));
-	};
-	document.head.appendChild(js);
+  return window.requestIdleCallback && window.IntersectionObserver;
 };
 
 if (browserSupportsAllFeatures()) {
-	main();
+  main();
 } else {
-	loadScript(
-		'https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=requestIdleCallback%2CIntersectionObserver',
-		main
-	);
+  loadScript(
+    'https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=requestIdleCallback%2CIntersectionObserver',
+    main
+  );
 }
---
+++ src/utils/loadScript.js
@@ -0,0 +1,7 @@
+export default loadScript = (src, done) => {
+  const js = document.createElement('script');
+  js.src = src;
+  js.onload = () => done();
+  js.onerror = () => done(new Error('Failed to load the script: ', src));
+  document.head.appendChild(js);
+};
